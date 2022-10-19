b1e7292c8ebd9bd523822acb95dbd8ac94f329a4
Daniel <danydu1299@gmail.com>
2022-10-19 10:56:17 -0500

    C# problems

---
+++ backend/c#_.NET/open-Bootcamp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Escribe tu nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine(nombre);
+
+string datetime = DateTime.Now.ToString("hh:mm:ss tt");
+Console.WriteLine(datetime);
